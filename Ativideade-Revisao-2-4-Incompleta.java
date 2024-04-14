976d27b2d9906936fbe558487d9aed0e54fe3cb5
Anderson-Klein <160682630+Anderson-Klein@users.noreply.github.com>
2024-04-14 17:13:03 -0300

    Create Ativideade-Revisao-2-4-Incompleta.java

---
+++ Ativideade-Revisao-2-4-Incompleta.java
@@ -0,0 +1,56 @@
+//Não consegui realisar este asercisio por falta de connhecimento, exemples e falta de mais tempo de aula
+
+package Objeto;
+
+public class Circulo {
+
+    private double raio;
+    private String cor;
+
+
+    public Circulo() {
+
+    }
+
+    public Circulo(double raio, String cor) {
+        this.raio = raio;
+        this.cor = cor;
+    }
+
+    public Circulo(double raio) {
+        this.raio = raio;
+    }
+
+    public Circulo(String cor) {
+        this.cor = cor;
+    }
+}
+
+
+//-----------------------------------------------------------------------------------------
+
+package Objeto;
+
+public class Circulo {
+
+    private double raio;
+    private String cor;
+
+
+    public Circulo() {
+
+    }
+
+    public Circulo(double raio, String cor) {
+        this.raio = raio;
+        this.cor = cor;
+    }
+
+    public Circulo(double raio) {
+        this.raio = raio;
+    }
+
+    public Circulo(String cor) {
+        this.cor = cor;
+    }
+}
